8967f75d6b3b0534101b943973b644a4fa044123
Gabriel Moura <gabriel.moura_hartbgroup@prestador.globo>
2022-06-01 23:34:05 -0300

    feat: web client initial structure

---
+++ Codigo/web/src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: '#00E78E',
+    secondary: '#000000',
+    background: '#F1F1F6',
+    text: '#737380',
+    margin: '#E1E3E5',
+    neutral100: '#FFFFFF',
+  },
+};
+
+export default theme;
